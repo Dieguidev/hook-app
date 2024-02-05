fa1a3283776aa05dda83644f2f170c89b3c5ddd4
Dieguidev <diegogaraycullas@gmail.com>
2024-02-05 18:33:23 -0500

    modificado CallbackHook.jsx para poder pasar argumentos a la funcion memorizada por useCallback

--- src/06-memos/CallbackHook.jsx
+++ src/06-memos/CallbackHook.jsx
@@ -8,8 +8,8 @@ export const CallbackHook = () => {
 
   //el useCallback sirve para memorizar funciones, es decir, cuando se llame a la misma función se va a guardar en memoria y cuando se llame a otra función se va a ejecutar la misma función guardada en memoria.
   const incrementFather = useCallback(
-    () => {
-      setCounter( (value) => value + 1 );
+    (value) => {
+      setCounter( (c) => c + value );
     },
     [],
   )
