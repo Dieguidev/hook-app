a6950a12cb51cea5a22c9032c589d454ac281856
Dieguidev <diegogaraycullas@gmail.com>
2024-02-05 18:05:26 -0500

    creado componente MemoHook.jsx para realizar un caso de uso de useMemo

--- src/main.jsx
+++ src/main.jsx
@@ -8,11 +8,12 @@ import './index.css'
 // import { MultipleCustmHooks } from './03-examples/MultipleCustmHooks'
 // import { FocusScreen } from './04-useRef/FocusScreen'
 // import { Layout } from './05-useLayoutEffect/Layout'
-import { Memorize } from './06-memos/Memorize'
+// import { Memorize } from './06-memos/Memorize'
+import { MemoHook } from './06-memos/MemoHook'
 // import { HooksApp } from './HooksApp.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Memorize/>
+    <MemoHook/>
   </React.StrictMode>,
 )
